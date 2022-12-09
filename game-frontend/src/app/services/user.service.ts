f5b1beef9630cc8b49cc495bcbf704285255fd59
jinhyoung kim <jinhyoung.kim@fdmgroup.com>
2022-12-09 12:04:05 -0500

    create user service

--- game-frontend/src/app/services/user.service.ts
+++ game-frontend/src/app/services/user.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../model/user/User';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  url:string="http://localhost:8080/api/v1/accounts"
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<User[]>{
+    return this.http.get<User[]>(this.url);
+  }
+
+  getUser(id: string): Observable<User>{
+    return this.http.get<User>(this.url+'/'+id);
+  }
+
+  createUser(user:User): Observable<User>{
+    return this.http.post<User>(this.url, user);
+  }
+
+  updateUser(user:User):Observable<User>{
+    return this.http.put<User>(this.url+'/'+ user.id, user);
+  }
+
+  deleteUser(id: string){
+    return this.http.delete(this.url+'/'+id);
+  }
 }
